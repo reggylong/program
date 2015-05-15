4a6d3291920b1a06598b9bfdc28bd265b96f6bc1
Colin Wei <colinwei@stanford.edu>
2015-05-15 05:05:56 -0700

    does something have to check if the tree is actually right though

--- data-processing/parseTree.py
+++ data-processing/parseTree.py
@@ -2,27 +2,51 @@ import collections
 import sys
 import os
 
-def TreeNode
-def convertExampleToTree(exFile):
+class ParseNode: # a node in the tree
+
+  def __init__(self,parseString,parent=None,children=[]):
+    self.parseString = parseString
+    self.parent = parent
+    self.children = children
+
+def convertExampleToTree(exFile, verbose=False):
   f = open(exFile, "r")
   totalTreeLines = []
   currTreeLines = []
+  treeList = []
   for line in f:
     if line.rstrip() == "": 
       totalTreeLines.append(currTreeLines)
+      currTreeLines = []
     else:
       currTreeLines.append(line)
   for tree in totalTreeLines: 
-    tabNumsOld = 1
-    root = TreeNode()
+    tabNumsOld = 0
+    root = ParseNode(tree[0].strip)
     currNode = root
-    root.parseString = tree[0].strip()
     for line in tree[1:]:
       tabNumsNew = len(line.split("\t")) - 1
+      parent = None
       if tabNumsNew == tabNumsOld: 
         parent = currNode.parent
-        newNode = 
-        parent.  
-     
-    
+      else: 
+        if tabNumsNew > tabNumsOld: 
+          parent = currNode
+        else: 
+          parent = currNode.parent.parent
+      newNode = ParseNode(line.strip(), parent=parent)
+      parent.children.append(newNode)
+      currNode = newNode
+      tabNumsOld = tabNumsNew
+    treeList.append(root)
+  return treeList
 
+def prepareTrainExamples(trainDir, verbose=False):
+  trainingSet = []
+  exampleFiles = [f for f in os.listdir(trainDir) if os.path.isfile(os.path.join(trainDir, f))]
+  for filename in exampleFiles: 
+    trainingSet.append(convertExampleToTree(os.path.join(trainDir, filename), verbose=verbose))
+    if verbose:
+      print("Finished example " + filename)
+if __name__ == "__main__":
+  prepareTrainExamples("../parses/iter.0", verbose=True)
