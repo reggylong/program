cee2f62a6910e6cd3f3f357273a8b71ef6e11bc0
Colin Wei <colinwei@stanford.edu>
2015-05-31 23:21:00 -0700

    language model added

--- testRecurrent.py
+++ testRecurrent.py
@@ -3,6 +3,8 @@ import pickle
 import os
 from dataprocessing.data_utils import *
 from dataprocessing.parseTree import ParseNode
+from linearembedding.simpleembedding import *
+from siamese.siamese import *
 from recurrentnn.rnn import *
 import itertools
 from numpy import *
@@ -10,7 +12,9 @@ from numpy import *
 args = sys.argv
 trainUtterSet = load_pickle(args[1])
 trainingExamples = load_pickle(args[2])
+print(trainingExamples[0])
 correctExamples = load_pickle(args[3])
+print(correctExamples[0])
 devUtterSet = load_pickle(args[4])
 zipAll = [(all, correct[0], utter) for all, correct, utter in zip(trainingExamples, correctExamples, trainUtterSet) if len(correct) > 0]
 trainingSet = [(all, correct) for all, correct, utter in zipAll]
@@ -20,41 +24,53 @@ devCorrect = load_pickle(args[6])
 zipAll = [(all, correct[0], utter) for all, correct, utter in zip(devExamples, devCorrect, devUtterSet) if len(correct) > 0]
 devSet = [(all, correct) for all, correct, utter in zipAll]
 devUtters = [utter for all, correct, utter in zipAll]
-vectorDim = int(args[7])
-saveFile = args[8]
+loadFile = args[7]
 #if len(args) > 9:
 #  loadFile = args[9]
-#  recurrent = load
+#  siamese = load
 worddict = load_pickle("data/word_to_index.pkl")
-
-def randgen(N, ntrain):
-    for i in range(N): 
-        yield random.randint(0, ntrain)
-
-def alphagen(N, alphastart):
-    curralpha = alphastart
-    print(curralpha)
-    for i in range(N):
-        if i % (N/2) == 0:
-            curralpha /= 3 
-        yield curralpha
-
-wv = sqrt(0.1)*random.standard_normal((len(worddict.keys()), vectorDim))
-recurrent = RNN(wv, alpha=0.002, bptt = 3)
-rand_gen = randgen(N=1000000, ntrain=50)
-alpha_gen = alphagen(N=1000000, alphastart = 0.002)
-recurrent.train_sgd(trainingSet, trainUtters, rand_gen, recurrent.annealiter(0.002, 300000), printevery=1000, costevery=10000)
-write_pickle("models/", recurrent, saveFile)
-predictions = recurrent.predict(devSet, devUtters)
-print("Dev loss: " + str(recurrent.compute_display_loss(devSet, devUtters)))
+inddict = {}
+for word in worddict.keys():
+  inddict[worddict[word]] = word
+  
+def parseToWords(parse1):
+  words1 = []
+  for ind in parse1[0]:
+    words1.append(inddict[ind])
+  words2 = []
+  for ind in parse1[1]:
+    words2.append(inddict[ind])
+  return words1, words2
+   
+network = load_pickle(loadFile)
+predictions = network.predict(devSet, devUtters)
+print("Dev loss: " + str(network.compute_display_loss(devSet, devUtters)))
 correct = 0
-trainpredictions = recurrent.predict(trainingSet, trainUtters)
+trainpredictions = network.predict(trainingSet, trainUtters)
 for ind, predict in enumerate(trainpredictions): 
     if checkParseEqual(trainingSet[ind][1], trainingSet[ind][0][predict]):
         correct += 1
+        print("************* EX " + str(ind))
+    else:
+        print("------------- EX " + str(ind))
+    print("Correct parse (train): ")
+    print(parseToWords(trainingSet[ind][1]))
+    print("Predicted parse: ")
+    print(parseToWords(trainingSet[ind][0][predict]))
+    print("Utterance: ")
+    print(parseToWords(trainUtters[ind]))
 print("Train accuracy " + str(float(correct)/len(trainpredictions)))
 correct = 0
 for ind, predict in enumerate(predictions): 
     if checkParseEqual(devSet[ind][1], devSet[ind][0][predict]):
         correct += 1
+        print("************* EX " + str(ind))
+    else: 
+        print("------------- EX " + str(ind))
+    print("Correct parse (dev): ") 
+    print(parseToWords(devSet[ind][1]))
+    print("Wrong parse: ")
+    print(parseToWords(devSet[ind][0][predict]))
+    print("Utterance: ")
+    print(parseToWords(devUtters[ind]))
 print("Accuracy " + str(float(correct)/len(predictions)))
